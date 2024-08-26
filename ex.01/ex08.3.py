7634f4bdc8137df64fe74a03d377bdc4ff6a4f2e
elisrael.oliveira <elisrael.oliveira@gmail.com>
2024-08-26 15:14:48 -0300

    commit

---
+++ ex.01/ex08.3.py
@@ -0,0 +1,10 @@
+idade = int(input("qual sua idade? "))
+
+if idade > 17:
+    print("você é de maior")
+
+elif idade == 17:
+    print("você em breve será de maior")
+
+else:
+    print("você é de menor")
