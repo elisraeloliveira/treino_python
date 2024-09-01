25ad11170a8ea4ea3cec1ff26edc59ad7fa9da08
elisrael.oliveira <elisrael.oliveira@gmail.com>
2024-09-01 20:20:54 -0300

    commit tuple

--- ex_03/ex03_tuple.py
+++ ex_03/ex03_tuple.py
@@ -7,3 +7,5 @@ letras = tuple("python") # inclui o constructor tuple
 numeros = tuple([1, 2, 3, 4]) # posso incluir também uma lista
 
 pais = ("Brasil",) # e incluir um elemento unico
+
+# as classes usadas no list é limitado a classes que não altere a tupla
