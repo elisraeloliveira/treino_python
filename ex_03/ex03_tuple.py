60836402a905f931e52c0a3efcdf5e2c9402c34d
elisrael.oliveira <elisrael.oliveira@gmail.com>
2024-09-01 20:11:59 -0300

    commit tuple

---
+++ ex_03/ex03_tuple.py
@@ -0,0 +1,9 @@
+# TUPLA é parecido com uma lista, porém imutável
+
+frutas = ("laranja","pera","uva",) # inclui uma vírgula no final
+
+letras = tuple("python") # inclui o constructor tuple
+
+numeros = tuple([1, 2, 3, 4]) # posso incluir também uma lista
+
+pais = ("Brasil",) # e incluir um elemento unico
