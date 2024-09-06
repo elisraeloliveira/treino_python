d437a27d69fc2dfd5ef484f79a49e8f37c70a80f
elisrael.oliveira <elisrael.oliveira@gmail.com>
2024-09-05 22:20:15 -0300

    commit date and hour

--- ex_04/ex01_date_and_hour.py
+++ ex_04/ex01_date_and_hour.py
@@ -1,1 +1,23 @@
-import
+import datetime
+import time
+
+# date
+
+d = datetime.date(2023, 7, 8)
+print(d)
+
+
+#outra maneira de usar o datatime. esta é a forma mais encontrada.
+
+#datetime
+from datetime import datetime
+
+data = datetime.today()
+print(data)
+
+# time
+
+import datetime
+
+hora = datetime.time(10,25,15)
+print(hora)
